08295d928fbfd84db8c0222e446be78fcfb1d0d1
Sandra <sandrakakkarayil01@gmail.com>
2021-05-17 13:55:54 +0530

    Added dashboard page

--- app/routes.py
+++ app/routes.py
@@ -8,15 +8,19 @@ from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
+    return render_template("index.html")
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
     threads = []
-    return render_template("index.html", title='Home Page', threads=threads)
+    return render_template("main.html", title='Home Page', threads=threads)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -26,7 +30,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('dashboard')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
@@ -39,7 +43,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('dashboard'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
