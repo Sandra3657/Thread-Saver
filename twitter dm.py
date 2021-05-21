b2b0cf8215ab70b63f9df1288f6753f22741e794
parvathyp301 <parvathyp07112000@gmail.com>
2021-05-21 17:09:53 +0530

    Twitter DM

---
+++ twitter dm.py
@@ -0,0 +1,24 @@
+import tweepy
+  
+# assign the values accordingly
+consumer_key = "consumer key"
+consumer_secret = "consumer secret"
+access_token = "access token"
+access_token_secret = "access token secret"
+  
+# authorization of consumer key and consumer secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  
+# set access to user's access key and access secret 
+auth.set_access_token(access_token, access_token_secret)
+  
+# calling the api 
+api = tweepy.API(auth)
+
+#To get ID of user
+user = api.get_user("thomas_akshaya")
+print(user.id_str)
+
+#sending direct message
+recipient_id = "1393800988004323328"  # ID of the user
+api.send_direct_message(recipient_id, "Hi")
